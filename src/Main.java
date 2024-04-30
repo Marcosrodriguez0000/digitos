b5d8314d1cc71c60daf5541e890b52799fc0db8e
Mañana <marcos04486@gmail.com>
2024-04-30 09:07:05 +0200

    Digitos

--- src/Main.java
+++ src/Main.java
@@ -6,43 +6,52 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (scanner.hasNext()) {
-            String input = scanner.nextLine();
-            if (input.equals("9999 ZZZ"))
+            String entrada = scanner.nextLine();
+            if (entrada.equals("9999 ZZZ"))
                 break;
-            String[] parts = input.split(" ");
-            int numberPart = Integer.parseInt(parts[0]);
-            String letterPart = parts[1];
+            String[] partes = entrada.split(" ");
+            int parteNumero = Integer.parseInt(partes[0]);
+            String parteLetra = partes[1];
 
 
-            String nextPlate = getNextPlate(numberPart, letterPart);
-            System.out.println(nextPlate);
+            String siguientePlaca = obtenerSiguientePlaca(parteNumero, parteLetra);
+            System.out.println(siguientePlaca);
         }
 
         scanner.close();
     }
 
-    public static String getNextPlate(int numberPart, String letterPart) {
-        numberPart++;
-        if (numberPart > 9999) {
-            numberPart = 0;
-            letterPart = incrementLetters(letterPart);
+    public static String obtenerSiguientePlaca(int parteNumero, String parteLetra) {
+        parteNumero++;
+        if (parteNumero > 9999) {
+            parteNumero = 0;
+            parteLetra = reiniciarLetras(parteLetra);
         }
-        return String.format("%04d %s", numberPart, letterPart);
+        return String.format("%04d %s", parteNumero, parteLetra);
     }
 
-    public static String incrementLetters(String letters) {
-        char[] chars = letters.toCharArray();
-        for (int i = chars.length - 1; i >= 0; i--) {
-            char c = chars[i];
-            if (c != 'Z') {
-                chars[i]++;
-                if (chars[i] == 'A' || chars[i] == 'E' || chars[i] == 'I' || chars[i] == 'O' || chars[i] == 'U')
-                    chars[i]++;
+    public static String reiniciarLetras(String letras) {
+        char[] caracteres = letras.toCharArray();
+        int carry = 1;
+        for (int i = caracteres.length - 1; i >= 0; i--) {
+            char c = caracteres[i];
+            if (carry == 0)
                 break;
+            if (c != 'Z') {
+                caracteres[i]++;
+                if (caracteres[i] == 'A' || caracteres[i] == 'E' || caracteres[i] == 'I' || caracteres[i] == 'O' || caracteres[i] == 'U')
+                    caracteres[i]++;
+                carry = 0;
             } else {
-                chars[i] = 'B';
+                caracteres[i] = 'B';
+                if (i > 0 && caracteres[i - 1] != 'Z') {
+                    caracteres[i - 1]++;
+                    if (caracteres[i - 1] == 'A' || caracteres[i - 1] == 'E' || caracteres[i - 1] == 'I' || caracteres[i - 1] == 'O' || caracteres[i - 1] == 'U')
+                        caracteres[i - 1]++;
+                }
+                carry = 0;
             }
         }
-        return new String(chars);
+        return new String(caracteres);
     }
 }
